62b21421b1dc4328d8df74054fb2a87ecebbf621
Sank-WoT <aserobabow95@mail.ru>
2015-06-30 00:28:58 +0600

    +Update 1.05

--- New Unity Project/Assets/Sword and blood/PrototypeCharacter/Tutorial/Ai1.cs
+++ New Unity Project/Assets/Sword and blood/PrototypeCharacter/Tutorial/Ai1.cs
@@ -1,30 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class Ai1 : MonoBehaviour 
 {
-	//Цель (пункт Б)
-	private GameObject mob;
-	public GameObject player;
-	public int Delay;
-	//Стартовая позиция
-	private Vector3 _startPos;
-	//Конечная позиция
-	private Vector3 _endPos;
-	// Use this for initialization
+	GameObject target; 
 	void Start ()
 	{
-		mob = (GameObject)this.gameObject; 
-
-		//Запоминаем начальную и конечную позиции
-		_startPos = mob.transform.position;
-	}
-	
-	// Update is called once per frame
+		target = GameObject.FindGameObjectWithTag("Player");
+	} 
 	void Update ()
 	{
-		_endPos = player.transform.position;
 		//Устанавливаем новую позицию
-		transform.position = Vector3.Slerp(_startPos, _endPos, Time.time/Delay);
+		transform.position = Vector3.Slerp (this.transform.position, target.transform.position, 1 * Time.deltaTime);
 	}
 }
