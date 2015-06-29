7b9c09b09c716cdc1383dd5b6e470c1d0e17004e
Sank-WoT <aserobabow95@mail.ru>
2015-06-29 20:58:58 +0600

    Update 1.04
    Poor movement mobs

---
+++ New Unity Project/Assets/Sword and blood/PrototypeCharacter/Tutorial/AutoAim.cs
@@ -0,0 +1,61 @@
+﻿/*using UnityEngine;
+using System.Collections.Generic;
+using FPSCTP;
+
+public class AutoAim : MonoBehaviour
+{
+	Vector3 nearestAim = Vector3.zero;
+	Vector3 dir, bestDir, enPos;
+	Transform myTransform;
+	float nearestAngle = 0;
+	public float MaxDistance = 35;
+	public float MinDistance = 0;
+	float distance = 0;
+	public float MaxAngle = 10f;
+	public float maxDegreesDelta = 3f;
+
+	void Start ()
+	{
+		myTransform = MainCharacter.MAIN_CHARACTER.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (OwnMouseLook.time > 0f) 
+		{
+			nearestAngle = 90;
+	
+			foreach (GenericEnemy enemy in GenericEnemy.enemyList) 
+			{
+				bestDir = Vector3.zero;
+				if (!enemy.GetDead ()) {
+
+					enPos = enemy.myTransform.position;
+					enPos.y += 1.3f;
+					dir = enPos - myTransform.position;
+					distance = Vector3.Distance (myTransform.position, enPos);
+					if (distance < MaxDistance && distance > MinDistance) {
+						if (nearestAngle > Vector3.Angle (myTransform.forward, dir)) 
+						{
+							nearestAngle = Vector3.Angle (myTransform.forward, dir);
+							bestDir = dir;
+						}
+
+					}
+
+				}
+				if (Vector3.Magnitude (bestDir) > 0.01f && nearestAngle < MaxAngle) 
+				{
+					myTransform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation (bestDir), maxDegreesDelta);
+				}
+
+
+			}
+		}
+
+	
+
+
+	}
+}*/
