1f466c68036ed0f785fe4d8478f1074cb135ba85
Денис <raidjinthunderkeg@yandex.ru>
2015-07-02 21:57:05 +0600

    Added InGame menu
    Created simple InGame menu, which can be call with button on GUI. InGame
    menu contain 3 buttons:
    Continue - return to game.
    Exit to main menu - close current game and load Menu scene.
    Quit game - close application.

---
+++ New Unity Project/Assets/Sword and blood/GUI/GameQuit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameQuit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Application.Quit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
